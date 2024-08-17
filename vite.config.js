c117728a00fddd71bf988afa5f5854447359c289
Jona <jonathan.diaz@driv.in>
2024-08-17 16:26:20 -0400

    update dependences

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://jedt89.github.io/Pizzeria_mamma_mia_3'
 })
